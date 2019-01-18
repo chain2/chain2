0f89b39d6b70090f7c18d5adf6228416f2b0a309
Tom Harding <tomh@thinlink.com>
2019-01-18 10:45:57 -0800

    Revert "Make push only mandatory when magnetic anomaly activates."
    This reverts commit a2cd24efb5dc82e6bf76f08a571a0a55dd32e610.

--- src/main.cpp
+++ src/main.cpp
@@ -1939,7 +1939,6 @@ bool ConnectBlock(const CBlock& block, CValidationState& state, CBlockIndex* pin
     }
 
     if (IsFourthHFActive(pindex->pprev->GetMedianTimePast())) {
-        flags |= SCRIPT_VERIFY_SIGPUSHONLY;
         flags |= SCRIPT_ENABLE_CHECKDATASIG;
     }
 
